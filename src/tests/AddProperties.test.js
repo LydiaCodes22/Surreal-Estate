4999d531259c80e73951c8eb78805fdb7bd001e1
Lydia Wallace <lydia.wallace@outlook.com>
2022-06-24 11:46:05 +0100

    adding testing for add properties form

--- src/tests/AddProperties.test.js
+++ src/tests/AddProperties.test.js
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import React from "react";
 import AddProperties from "../components/AddProperties";
 
@@ -7,5 +7,21 @@ describe("the add properties component", () => {
     const { asFragment } = render(<AddProperties />);
 
     expect(asFragment()).toMatchSnapshot();
+  });
+  it("should render basic fields", () => {
+    render(<AddProperties />);
+    expect(screen.getByRole("heading", { name: /add/i })).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: /add/i })).toBeInTheDocument();
+    expect(screen.getByRole("textbox", { name: /title/i })).toBeInTheDocument();
+    expect(screen.getByRole("textbox", { name: /type/i })).toBeInTheDocument();
+    expect(
+      screen.getByRole("textbox", { name: /bedrooms/i })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("textbox", { name: /bathrooms/i })
+    ).toBeInTheDocument();
+    expect(screen.getByRole("textbox", { name: /price/i })).toBeInTheDocument();
+    expect(screen.getByRole("textbox", { name: /city/i })).toBeInTheDocument();
+    expect(screen.getByRole("textbox", { name: /email/i })).toBeInTheDocument();
   });
 });
